8228f15696a9a73e2acb5b0aef5df535cea57265
Colin Gormley <colin.gormley@ed.ac.uk>
2014-04-17 10:58:15 +0100

    Issue #1: Tidied up and removed old code.

--- platforms/ios/CustomCamera/Classes/CustomCameraViewController.m
+++ platforms/ios/CustomCamera/Classes/CustomCameraViewController.m
@@ -43,9 +43,9 @@
 	// Call the takePicture method on the UIImagePickerController to capture the image.
 	[self.picker takePicture];
 }
--(void)image:(UIImage *)image
-finishedSavingWithError:(NSError *)error
- contextInfo:(void *)contextInfo
+
+// Deal with error taking photo
+-(void)image:(UIImage *)image finishedSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
 {
     if (error) {
         UIAlertView *alert = [[UIAlertView alloc]
@@ -57,60 +57,37 @@ finishedSavingWithError:(NSError *)error
         [alert show];
     }
 }
+
 // Delegate method.  UIImagePickerController will call this method as soon as the image captured above is ready to be processed.  This is also like an event callback in JavaScript.
 -(void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
 	// Get a reference to the captured image
 	UIImage* image = [info objectForKey:UIImagePickerControllerOriginalImage];
-  	// Get a file path to save the JPEG
-//	NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-//	NSString* documentsDirectory = [paths objectAtIndex:0];
-//	NSString* filename = @"test.jpg";
-//	NSString* imagePath = [documentsDirectory stringByAppendingPathComponent:filename];
     
-    // New Addition - write to photo gallery
+    
+    // Write to photo gallery
     UIImageWriteToSavedPhotosAlbum(image,
                                    self,
                                    @selector(image:finishedSavingWithError:contextInfo:),
                                    nil);
     
-    
-    
+    //TODO: write to assets
     NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
-    NSError* err = nil;
+    
     NSFileManager* fileMgr = [[NSFileManager alloc] init]; // recommended by apple (vs [NSFileManager defaultManager]) to be threadsafe
     // generate unique file name
     NSString* filePath;
     
     int i = 1;
     do {
-    filePath = [NSString stringWithFormat:@"%@/%s%03d.%s", docsPath, "cdv_photo_", i++ ,"jpg"];
+        filePath = [NSString stringWithFormat:@"%@/%s%03d.%s", docsPath, "cdv_photo_", i++ ,"jpg"];
     } while ([fileMgr fileExistsAtPath:filePath]);{
         // Get the image data (blocking; around 1 second)
         NSData* imageData = UIImageJPEGRepresentation(image, 0.5);
-     [imageData writeToFile:filePath atomically:YES];
-    
+        [imageData writeToFile:filePath atomically:YES];
     }
-   /* ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-    [library writeImageToSavedPhotosAlbum:[image CGImage] orientation:(ALAssetOrientation)[image imageOrientation] completionBlock:^(NSURL *assetURL, NSError *error){
-        if (error) {
-            NSLog(@"error");
-        } else {
-            NSLog(@"url %@", assetURL);
-        }
-    }];*/
-   //[library release];
-    
-	// Get the image data (blocking; around 1 second)
-	//NSData* imageData = UIImageJPEGRepresentation(image, 0.5);
-    
-	// Write the data to the file
-	//[imageData writeToFile:imagePath atomically:YES];
-    
-    //	[imageData writeToFile:filePath atomically:YES];
     
 	// Tell the plugin class that we're finished processing the image
-	//[self.plugin capturedImageWithPath:imagePath];
     [self.plugin capturedImageWithPath:filePath];
 }
 
